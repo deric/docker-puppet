da052499251e62b04bf655ffe06bc1322336f37c
Tomas Barton <tomas.barton@recombee.com>
2016-11-19 07:15:02 +0000

    puppet 4 installation

--- jessie-puppet/install.sh
+++ jessie-puppet/install.sh
@@ -7,10 +7,18 @@ echo "version=${version}"
 a=( ${version//./ } )   # replace points, split into array
 
 
-# for puppet 3.8.x and older
-gem install puppet -v ${version}
 gem install deep_merge
 if [ ${a[0]} -ge 4 ]; then
-  # TODO: some extra work for puppet4?
-  echo $(puppet --version)
+  codename=$(lsb_release -c | awk -v FS=":\t" '/Codename/ {print $2}')
+  wget -O /tmp/puppet.deb http://apt.puppetlabs.com/puppetlabs-release-pc1-${codename}.deb -O /tmp/puppet.deb
+  dpkg -i --force-all /tmp/puppet.deb
+  apt-get update && apt-get install --force-yes  -y puppet-agent
+  mkdir /root/.ssh/
+  cat <<EOF > ~/.ssh/environment
+PATH=/opt/puppetlabs/bin:/opt/puppetlabs/bin/hiera:/usr/bin:/opt/puppetlabs/bin/hiera:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:$PATH
+EOF
+  rm -r /etc/puppetlabs/code/modules && ln -s /etc/puppet/modules /etc/puppetlabs/code/modules
+else
+  # for puppet 3.8.x and older
+  gem install puppet -v ${version}
 fi
