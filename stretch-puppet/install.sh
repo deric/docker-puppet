4f4a818bec724ee3c8ee08f2670bad9d9b16526f
Tomas Barton <barton.tomas@gmail.com>
2019-05-16 18:24:32 +0200

    attempts to install exactly the same version

--- stretch-puppet/install.sh
+++ stretch-puppet/install.sh
@@ -4,12 +4,13 @@ set -e
 
 version=$1
 echo "version=${version}"
+minor=1
 a=( ${version//./ } )   # replace points, split into array
 
 codename=$(lsb_release -c | awk -v FS=":\t" '/Codename/ {print $2}')
 wget -O /tmp/puppet.deb http://apt.puppetlabs.com/puppet${a[0]}-release-${codename}.deb -O /tmp/puppet.deb
 dpkg -i --force-all /tmp/puppet.deb
-apt-get update && apt-get install -y puppet-agent
+apt-get update && apt-get install -y puppet-agent=${version}-${minor}${codename}
 mkdir /root/.ssh/
 cat <<EOF > ~/.ssh/environment
 PATH=/opt/puppetlabs/bin:/opt/puppetlabs/bin/hiera:/usr/bin:/opt/puppetlabs/bin/hiera:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:$PATH
