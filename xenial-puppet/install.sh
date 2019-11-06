ef19d65638fe43012b8f2b8a3bf1a2d06c067d29
Tomas Barton <barton.tomas@gmail.com>
2019-11-06 18:20:00 +0100

    fix path for beaker

--- xenial-puppet/install.sh
+++ xenial-puppet/install.sh
@@ -13,7 +13,7 @@ dpkg -i --force-all /tmp/puppet.deb
 apt-get update && apt-get install -y puppet-agent=${version}-${minor}${codename}
 mkdir /root/.ssh/
 cat <<EOF > ~/.ssh/environment
-export PATH=$PATH:/opt/puppetlabs/bin:/opt/puppetlabs/bin/hiera:/usr/bin:/opt/puppetlabs/bin/hiera:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:$PATH
+PATH=/opt/puppetlabs/bin:/opt/puppetlabs/bin/hiera:/usr/bin:/opt/puppetlabs/bin/hiera:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:$PATH
 EOF
 rm -r /etc/puppetlabs/code/modules && ln -s /etc/puppet/modules /etc/puppetlabs/code/modules
 rm /tmp/puppet.deb
