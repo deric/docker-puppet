8dadb13144f310622363757137fb0404db7ccbb1
Tomas Barton <barton.tomas@gmail.com>
2019-11-07 11:11:39 +0100

    support centos8

---
+++ centos8-puppet/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# This bootstraps Puppet on RedHat/Centos
+set -e
+
+version=$1
+echo "version=${version}"
+minor=1
+a=( ${version//./ } )   # replace points, split into array
+
+# gets major dist version e.g. 6/7/8
+major=$(rpm -E %{rhel})
+wget -O /tmp/puppet.rpm http://yum.puppetlabs.com/puppet${a[0]}-release-el-${major}.noarch.rpm
+rpm -ivh /tmp/puppet.rpm
+yum -y update && yum install -y puppet-agent-${version}-${minor}.el${major}
+mkdir /root/.ssh/
+cat <<EOF > ~/.ssh/environment
+PATH=/opt/puppetlabs/bin:/opt/puppetlabs/bin/hiera:/usr/bin:/opt/puppetlabs/bin/hiera:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:$PATH
+EOF
+mkdir /etc/puppet && ln -s /etc/puppet/modules /etc/puppetlabs/code/modules
+rm /tmp/puppet.rpm
